f7b07edd0700736d389274164ef8c835453bb2c1
Airath <ferenc.olah1993@gmail.com>
2025-04-13 15:08:34 +0200

    design elkezdése

--- main.js
+++ main.js
@@ -1,53 +1,98 @@
-const megoldasok = {
-  fel1_1: "en",
-  fel1_2: "e",
-  fel1_3: "st",
-  fel1_4: "en",
-  fel1_5: "t",
-  fel1_6: "t",
-};
+import { megoldasok, megoldasok2, megoldasok3 } from "./megoldasok.js";
 
 // Callback függvény az ellenőrzéshez
-function ellenorzo(inputElem, key) {
-  const ertek = inputElem.value.trim(); // Az input értéke, levágott szóközökkel
-  inputElem.classList.remove("helyes", "rossz"); // Előző osztályok eltávolítása
+function ellenorzo(megoldasok, inputElem, key) {
+  const ertek = inputElem.value.trim();
+  inputElem.classList.remove("helyes", "rossz");
 
-  // Ha az input üres, akkor is rossz válasznak számít
   if (ertek === "") {
     inputElem.classList.add("rossz");
   } else if (ertek === megoldasok[key]) {
-    inputElem.classList.add("helyes"); // Helyes válasz esetén
+    inputElem.classList.add("helyes");
   } else {
-    inputElem.classList.add("rossz"); // Rossz válasz esetén
+    inputElem.classList.add("rossz");
   }
 }
 
-function processInputs(callback) {
+// Input mezők feldolgozása
+function processInputs(megoldasok, callback) {
   Object.keys(megoldasok).forEach((key) => {
     const inputElem = document.querySelector(`.${key}`);
-    if (inputElem) callback(inputElem, key); // Meghívja a callbacket az input mezőn
+    if (inputElem) callback(megoldasok, inputElem, key);
   });
 }
 
-// Ellenőrzés gomb
-document.querySelector(".javito_btn").addEventListener("click", () => {
-  processInputs(ellenorzo);
-});
-
-// Reset gomb
-function resetInputs(inputElem) {
-  inputElem.value = ""; // Az input értékének törlése
-  inputElem.classList.remove("helyes", "rossz"); // Az osztályok eltávolítása
+// Reset művelet
+function resetInputs(_, inputElem) {
+  inputElem.value = "";
+  inputElem.classList.remove("helyes", "rossz");
 }
-function felMegoldasok(inputElem, key) {
+
+// Megoldások megjelenítése
+function felMegoldasok(megoldasok, inputElem, key) {
   inputElem.classList.remove("helyes", "rossz");
   inputElem.value = megoldasok[key];
 }
 
-document.querySelector(".reset_btn").addEventListener("click", () => {
-  processInputs(resetInputs);
+// Művelet- és megoldásleképezés
+const muveletek = {
+  ellenorzo,
+  reset: resetInputs,
+  megoldas: felMegoldasok,
+};
+
+const megoldasokMap = {
+  1: megoldasok,
+  2: megoldasok2,
+  3: megoldasok3,
+};
+
+// Dinamikus eseményfigyelés minden gombra
+document.querySelectorAll(".btn").forEach((gomb) => {
+  gomb.addEventListener("click", (event) => {
+    const megoldasKey = event.target.dataset.megoldas;
+    const muveletKey = event.target.dataset.muvelet;
+
+    const aktivMegoldas = megoldasokMap[megoldasKey];
+    const callback = muveletek[muveletKey];
+
+    if (aktivMegoldas && callback) {
+      processInputs(aktivMegoldas, callback);
+    } else {
+      console.warn(
+        "Hibás vagy hiányzó data attribútum:",
+        megoldasKey,
+        muveletKey
+      );
+    }
+  });
 });
 
-document.querySelector(".megoldas_btn").addEventListener("click", () => {
-  processInputs(felMegoldasok);
+// Valami
+
+const boxes = document.querySelectorAll(".box");
+let currentBox = null;
+let offsetX, offsetY;
+
+boxes.forEach((box) => {
+  box.addEventListener("mousedown", (e) => {
+    currentBox = box;
+    offsetX = e.clientX - box.offsetLeft;
+    offsetY = e.clientY - box.offsetTop;
+    box.style.cursor = "grabbing";
+  });
+});
+
+document.addEventListener("mousemove", (e) => {
+  if (currentBox) {
+    currentBox.style.left = `${e.clientX - offsetX}px`;
+    currentBox.style.top = `${e.clientY - offsetY}px`;
+  }
+});
+
+document.addEventListener("mouseup", () => {
+  if (currentBox) {
+    currentBox.style.cursor = "grab";
+    currentBox = null;
+  }
 });
---
+++ megoldasok.js
@@ -0,0 +1,38 @@
+export const megoldasok = {
+  // 1. Feladat
+
+  fel1_1: "en",
+  fel1_2: "e",
+  fel1_3: "st",
+  fel1_4: "en",
+  fel1_5: "t",
+  fel1_6: "t",
+};
+
+export const megoldasok2 = {
+  // 2. Feladat
+
+  fel2_1: "wäscht",
+  fel2_2: "Läufst",
+  fel2_3: "schläft",
+  fel2_4: "lese",
+  fel2_5: "fahren",
+  fel2_6: "weiß",
+  fel2_7: "nimmt",
+  fel2_8: "tragen",
+  fel2_9: "gibt",
+  fel2_10: "Weißt",
+};
+
+export const megoldasok3 = {
+  fel3_1: "t",
+  fel3_2: "t",
+  fel3_3: "e",
+  fel3_4: "t",
+  fel3_5: "et",
+  fel3_6: "t",
+  fel3_7: "et",
+  fel3_8: "en",
+  fel3_9: "t",
+  fel3_10: "et",
+};
